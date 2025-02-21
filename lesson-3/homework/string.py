613fc9326c308aa7bd9fa0f2acc9e77550931568
BahodirKodirov <bqodirov291@gmail.com>
2025-02-21 23:20:25 +0500

    Create string.py

---
+++ lesson-3/homework/string.py
@@ -0,0 +1,32 @@
+# Given two integer variables: a and b. Swap the values of the variables.
+# a = int(input())
+# b = int(input())
+
+# write code here. Don't change given conditions
+
+# print(a, b)
+
+# Solve this problem in 3 ways
+a = int(input())
+b = int(input())
+print(a,b)
+a,b=b,a
+print(a,b)
+
+
+x = int(input())
+y = int(input())
+l=[x,y]
+l
+l.reverse() 
+l
+
+
+
+c = int(input())
+d = int(input())
+temp = c
+c = d
+d = temp
+
+print(c, d)
